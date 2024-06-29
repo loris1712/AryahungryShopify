69cc0639bb4f6034588b4d02b545dd09217915e7
Archibold Bernard <aephisarh@gmail.com>
2024-06-29 06:59:08 +0000

    form section id

--- assets/custom-form.js
+++ assets/custom-form.js
@@ -1,10 +1,11 @@
-function onSubmit() {
+function onSubmit(sectionId) {
     const emailAddress = document.getElementById('destinationEmail').value;
   const name = document.getElementById('form-input-name').value;
   const phone = document.getElementById('form-input-phone').value;
   const message = document.getElementById('form-input-message').value;
   
   const payload = {
+    sectionId,
     emailAddress,
     name,
     phone,
