a7f4c7dfeee057b2109513172e7fd482ff1b3aac
Archibold Bernard <aephisarh@gmail.com>
2024-06-29 07:15:33 +0000

    query form values with id

--- assets/custom-form.js
+++ assets/custom-form.js
@@ -1,9 +1,13 @@
 function onSubmit(sectionId) {
-    const emailAddress = document.getElementById('destinationEmail').value;
-  const name = document.getElementById('form-input-name').value;
-  const phone = document.getElementById('form-input-phone').value;
-  const message = document.getElementById('form-input-message').value;
-  
+  const emailAddress = document.getElementById(
+    `${sectionId}-destinationEmail`,
+  )?.value;
+  const name = document.getElementById(`${sectionId}-form-input-name`)?.value;
+  const phone = document.getElementById(`${sectionId}-form-input-phone`)?.value;
+  const message = document.getElementById(
+    `${sectionId}-form-input-message`,
+  )?.value;
+
   const payload = {
     sectionId,
     emailAddress,
