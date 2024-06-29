86dac6357bbc7f111cb359afe4bffddd568043ac
Archibold Bernard <aephisarh@gmail.com>
2024-06-29 06:15:47 +0000

    access destination email

--- assets/custom-form.js
+++ assets/custom-form.js
@@ -1,9 +1,11 @@
 function onSubmit() {
+    const emailAddress = '{{ section.setting.emailDestination }}'; ;
   const name = document.getElementById('form-input-name');
   const phone = document.getElementById('form-input-phone');
   const message = document.getElementById('form-input-message');
   
   const payload = {
+    emailAddress,
     name,
     phone,
     message,
