4fb0eac83840971e264fa2fb013e3951ce5b2894
Archibold Bernard <aephisarh@gmail.com>
2024-06-29 06:01:54 +0000

    add email destination

--- assets/custom-form.js
+++ assets/custom-form.js
@@ -2,11 +2,12 @@ function onSubmit() {
   const name = document.getElementById('form-input-name');
   const phone = document.getElementById('form-input-phone');
   const message = document.getElementById('form-input-message');
-  console.log(name);
-  console.log(phone);
-  console.log(message);
+  
+  const payload = {
+    name,
+    phone,
+    message,
+  };
 
-  if (name) {
-    alert(name.value);
-  }
+  console.log(payload);
 }
