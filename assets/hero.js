8a2b543bf31056e427138d9ee71165352094fb8e
Archibold Bernard <aephisarh@gmail.com>
2024-06-30 08:35:51 +0000

    assign to input

--- assets/hero.js
+++ assets/hero.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("LOADED")
+    const dateInput = document.getElementById('herodateinput').value ?? "";
+    var currentDate = moment.now().format('MM/DD/YYYY hh:mm:ss');
+    console.log({ releaseDate, currentDate, dateInput });
 })
