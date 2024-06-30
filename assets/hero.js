ee4b6f80d3f1e90ee3046457db2ceb348c5105cd
Archibold Bernard <aephisarh@gmail.com>
2024-06-30 08:40:10 +0000

    banner

--- assets/hero.js
+++ assets/hero.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("LOADING")
     const dateInput = document.getElementById('herodateinput').value ?? "";
     var currentDate = moment.now().format('MM/DD/YYYY hh:mm:ss');
-    console.log({ releaseDate, currentDate, dateInput });
+    console.log({ currentDate, dateInput });
+
+    const bannerDate = document.getElementById('banner-date');
+    bannerDate.innerHTML = "WELCOME";
+        console.log('DONE');
+
 })
