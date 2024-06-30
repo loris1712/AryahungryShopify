7a53a3083a67865e85275dcbc7a7544c0230f2f1
Archibold Bernard <aephisarh@gmail.com>
2024-06-30 07:08:52 +0000

    liquid date loaded

--- assets/hero.js
+++ assets/hero.js
@@ -1,5 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var releaseDate = '{{ release_date }}';
-    var currentDate = new Date();
-    console.log({ releaseDate, currentDate });
+    console.log("LOADED")
 })
